365feac779628bcc5e936a5d3a91dea8facef871
elian-sss <elian.silva.if@gmail.com>
2025-09-16 19:30:45 -0300

    fix: alguns ajustes

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -11,23 +11,13 @@ import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Feed',
         href: dashboard(),
         icon: LayoutGrid,
     },
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
 ];
 </script>
 
